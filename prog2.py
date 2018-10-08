80b009fd9322bac988ec1e66a9356eb9c97c9553
Landon <laba2346@colorado.edu>
2018-10-08 13:13:19 -0600

    implemented changes for the calc_pred_load function, per email convo

--- prog2.py
+++ prog2.py
@@ -1,4 +1,5 @@
 from config import *
+import random
 
 def main():
     load, price = simulate()
@@ -8,8 +9,12 @@ def main():
 # This equation was derived from a sample data set spanning 24 hours, with an
 # hour between data points. Adjusted to allow for values of t that represent
 # 15 minute intervals.
-def calc_pred_load(t):
-    return (2.2/16)*t**2 + (73/4)*t + (1188/4)
+def calc_pred_load(t, act_load):
+    #return (2.2/16)*t**2 + (73/4)*t + (1188/4)
+    if(t > 0):
+        return act_load[t-1] + random.randint(-100, 100)
+    else:
+        return 500
 
 def simulate():
     # Keep track of actual price and load of the system over a 24 hour period,
@@ -17,7 +22,7 @@ def simulate():
     act_load = []
     act_price = []
     for t in range(0, 96):
-        pred_load = calc_pred_load(t)
+        pred_load = calc_pred_load(t, act_load)
         pred_price = (2.2)*a*pred_load + b
 
         # If we are beyond the first time step and have data for act_price,
